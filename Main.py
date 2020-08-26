b46b0d8dc3daa8f9864774e2c325f2d1c3e348fe
Alexnader Watermen <AlexanderWatermenAWC@gmail.com>
2020-08-26 13:34:25 -0400

    Added a file ending format and updated reuseablity

--- Main.py
+++ Main.py
@@ -1,10 +1,14 @@
 #higher scale = larger files/images and better quality
-img_scale = 40
+img_scale = 4
+
+fileName = 'test2.JPG'
+
+gen_type = 'single'
 
 from PIL import Image, ImageDraw
 
 # get pixel data
-im = Image.open('test.jpg', 'r')
+im = Image.open(fileName, 'r')
 width, height = im.size
 pixel_values = list(im.getdata())
 
@@ -16,30 +20,106 @@ draw = ImageDraw.Draw(blank)
 def create_circle(x, y, r): #center coordinates, radius
     draw.ellipse((x-r, y-r, x+r, y+r), fill=(0,0,0,255))
 
-print ("start")
-perc = 0
-# for every pixel get the index and rgb value
-for index, values in enumerate(pixel_values):
-    # skip every other line
-    if (index%width) % 2 == 0 or int(index/width) % 2 == 0:
-        continue
+# types of generation
+types = ['single','quad']
 
-    # calculacte gray value (inverted)
-    gray_value = (values[0] * 0.3) + (values[1] * 0.59) + (0.11 * values[2]) # (max(values) + min(values)) / 2 #
+# generate the image
+def generate():
+    if gen_type not in types:
+        raise Exception("Please chose a valid type") 
+        print (types)
+
+
+    print ("start")
+    perc = 0
+
+
+    if gen_type == 'single':
+        # for every pixel get the index and rgb value
+        for index, values in enumerate(pixel_values):
+            # skip every other line
+            if (index%width) % 2 == 0 or int(index/width) % 2 == 0:
+                continue
+
+            # calculacte gray value (inverted)
+            gray_value = (values[0] * 0.3) + (values[1] * 0.59) + (0.11 * values[2]) + 1# (max(values) + min(values)) / 2 #
+            
+            # get x and y pos
+            y, x = (index/width), (index%width)
+
+
+            
+            # draw circle at correct positions and add invert gray scale (to non inverted)
+            # print(gray_value)
+            create_circle(x * img_scale, y * img_scale, (1 - float((gray_value)/255))*img_scale)
+
+            #loading ani
+            if (int(index/len(pixel_values) * 100) > perc):
+                perc = (int(index/len(pixel_values) * 100))
+                print(str(perc) + "%")
+
+
+
+
+
+    elif gen_type == 'quad':
+        # for every pixel get the index and rgb value
+        for index, values in enumerate(pixel_values):
+            # skip every other line
+            '''if (index%width) % 2 == 0 or int(index/width) % 2 == 0:
+                    continue
+            '''
+            # calculacte gray value (inverted)
+            gray_value = (values[0] * 0.3) + (values[1] * 0.59) + (0.11 * values[2]) + 1# (max(values) + min(values)) / 2 #
+            
+            # get x and y pos
+        
+            y, x = (index/width) * img_scale, (index%width) * img_scale
+
+            # draw circle at correct positions and add invert gray scale (to non inverted)
+            # print(gray_value)
+            
+            for i in range(img_scale):
+                for j in range(img_scale):
+                    create_circle(
+                                x + i, 
+                                y + j, 
+                                (1 - float((gray_value)/255))*img_scale/2)
+
+            #loading ani
+            if (int(index/len(pixel_values) * 100) > perc):
+                perc = (int(index/len(pixel_values) * 100))
+                print(str(perc) + "%")
+
+            
+
+    # show the img
+    blank.show()
+
+    #save the img
+    blank.save(str(fileName.split('.')[-2]) + '.' + str(gen_type) + '.' + str(img_scale) + '.output.png')
+
+
+
+
+
+if __name__ == '__main__':
+    generate()
+
+'''
+    values = pixel_values[int(len(pixel_values)/2)]
+
+    gray_value = (values[0] * 0.3) + (values[1] * 0.59) + (0.11 * values[2]) + 1
     
-    # get x and y pos
-    y, x = (index/width), (index%width)
 
-    # draw circle at correct positions and add invert gray scale (to non inverted)
-    create_circle(x * img_scale, y * img_scale, (1 - gray_value/255) * img_scale)
+    y, x = (height/2), (width/2)
 
-    #loading ani
-    if (int(index/len(pixel_values) * 100) > perc):
-        perc = (int(index/len(pixel_values) * 100))
-        print(str(perc) + "%")
+    create_circle(x, y, (1 - float((gray_value)/255)))
 
-# show the img
-blank.show()
+    print(gray_value)
+    print(1 - float((gray_value)/255))
 
-#save the img
-blank.save('output.png')
+
+
+    blank.show()
+    '''
--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # img_to_dots
 Takes an image as an input and recreates it in dot forms
 
+The only image i have the rights to is 'test2.jpg'. 
 
 ways to improve:
 - dot to pixel size decrease (more dots per pixel but keep the same resolution)
 - multithreading 
 - faster language
+- decrease image upscale
+
